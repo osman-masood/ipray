2abe9c62e3a2381a4fb476822a1dfdba9440f1e7
Osman Masood <oamasood@gmail.com>
2013-04-14 19:51:45 -0700

    reading from ipray.html

--- web.go
+++ web.go
@@ -3,12 +3,17 @@ package main
 import (
   "code.google.com/p/goweb/goweb"
   "fmt"
+  "io/ioutil"
 )
 
 func main() {
+  pageRead, error := ioutil.ReadFile("ipray.html")
+  if error != nil {
+    panic(error.String())
+  }
 
   goweb.MapFunc("/", func(c *goweb.Context) {
-    fmt.Fprintf(c.ResponseWriter, '', )
+    fmt.Fprintf(c.ResponseWriter, string(pageRead))
   })
 
   goweb.ListenAndServe(":80")
